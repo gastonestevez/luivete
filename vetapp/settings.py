195d14d5d0577e595897d7ba4d5b94545f3ac825
Gastón Estévez <gastonestevez@Gastons-MacBook-Air-2.local>
2018-06-09 12:00:33 -0300

    static_url

--- vetapp/settings.py
+++ vetapp/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TABBED_ADMIN_USE_JQUERY_UI = True
