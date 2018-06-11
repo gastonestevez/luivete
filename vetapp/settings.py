ea615af89b31840730a430fb6c6f6f1c19b530a4
Gastón Estévez <gastonestevez@Gastons-MacBook-Air-2.local>
2018-06-11 00:51:19 -0300

    cambio a postgresql probando..

--- vetapp/settings.py
+++ vetapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,6 +88,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
