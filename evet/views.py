522bf12653e277c9f9b235609ed8d469414839a3
gastonestevez <gaston.estevez7@gmail.com>
2018-06-19 14:40:03 -0300

    Add files via upload
    cambio de views

--- evet/views.py
+++ evet/views.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from models import Turno
-
+import datetime
+from datetime import timedelta
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    turnos = Turno.objects.all()
+    startdate = datetime.datetime.now().date()
+    enddate = startdate + timedelta(days=365)
+    turnos = Turno.objects.filter(fecha__range=[startdate, enddate])
     return render(request,'evet/turnos.html', {'turnos':turnos})
 
 def modificarTurno(request):
