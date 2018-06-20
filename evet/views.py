938ff739404096500137a7fd52572366d67e701b
Gastón Estévez <gastonestevez@Gastons-MacBook-Air-2.local>
2018-06-20 17:04:49 -0300

    agrego models a views para heroku

--- evet/views.py
+++ evet/views.py
@@ -5,6 +5,8 @@ import datetime
 from datetime import timedelta
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+
 # Create your views here.
 
 def index(request):
